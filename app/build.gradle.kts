2bafd0fb1e2f82814f5a8e100c9740274e183a87
Herman Cheung <herman.kh.cheung@gmail.com>
2020-05-13 17:43:34 +0800

    add activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,3 @@
-
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -9,7 +8,7 @@ plugins {
 android {
     compileSdkVersion(28)
     defaultConfig {
-        applicationId = "io.hkhc.ihlog"
+        applicationId = "io.hkhc.mylibrary.demo"
         minSdkVersion(21)
         targetSdkVersion(28)
         versionCode = 1
@@ -28,6 +27,8 @@ dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation(kotlin("stdlib-jdk8", "1.3.70"))
     implementation("com.android.support:appcompat-v7:28.0.0")
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
     testImplementation("junit:junit:4.12")
     androidTestImplementation("com.android.support.test:runner:1.0.2")
     androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2")
